8949eeedaa5eea4885ef148ad5e2c8566f726cd0
unes007 <42010429+unes007@users.noreply.github.com>
2026-07-02 22:58:11 +0200

    Implement RadioPlaybackService with ExoPlayer

---
+++ app/src/main/java/com/example/radiomaroc/RadioPlaybackService.kt
@@ -0,0 +1,31 @@
+package com.example.radiomaroc
+
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
+
+@UnstableApi
+class RadioPlaybackService : MediaSessionService() {
+
+    private var mediaSession: MediaSession? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        val player = ExoPlayer.Builder(this).build()
+        mediaSession = MediaSession.Builder(this, player).build()
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+        return mediaSession
+    }
+
+    override fun onDestroy() {
+        mediaSession?.run {
+            player.release()
+            release()
+            mediaSession = null
+        }
+        super.onDestroy()
+    }
+}
